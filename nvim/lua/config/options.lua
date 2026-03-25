7f218ab70668df4b6fdb801beef53005097047f9
MLNelsson <mikael.nelsson@live.se>
2026-03-25 21:32:51 +0100

    Update some key binds

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,4 +3,21 @@ vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.termguicolors = true
 vim.opt.listchars = { space = '·', tab = '>-', trail = '-', nbsp = '%' }
-vim.opt.list = true
+vim.opt.list = tru
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+    local changed = false
+    for i, line in ipairs(lines) do
+      local trimmed = line:gsub("%s+$", ""):gsub("\r", "")
+      if trimmed ~= line then
+        lines[i] = trimmed
+        changed = true
+      end
+    end
+    if changed then
+      vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+    end
+  end,
+})
